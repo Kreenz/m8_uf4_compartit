2efdb99b68b267899cfe47ae7713253f63303672
oriolfornos <oriol-1-2@hotmail.com>
2020-10-02 15:56:42 +0200

    Primera pujada del codi del index.php

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        
+             /* function ofc_contar(){
+                
+            }
+            
+            function ofc_salida(){
+                
+            }
+            
+            function ofc_entrada(){
+                
+            } */
+            
+        ?>
+    </body>
+</html>
